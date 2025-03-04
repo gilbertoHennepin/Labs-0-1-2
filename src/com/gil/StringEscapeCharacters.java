78770de5b38d3c71c7c1c646b874639386fea3a7
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-04 11:11:02 -0600

    Update StringEscapeCharacters.java

--- src/com/gil/StringEscapeCharacters.java
+++ src/com/gil/StringEscapeCharacters.java
@@ -1,4 +1,12 @@
 package com.gil;
 
 public class StringEscapeCharacters {
+    public static void main(String[] args)  { //"psvm" shortcut
+        String message = "This class is \"Java Programming\"!";  // use "\" in between text to add "" when program runs
+            System.out.println(message);
+        String anotherMessage = "Welcome, everyone!\nI hope you enjoy the class!"; // use "\n" to add a new line
+            System.out.println(anotherMessage);
+        String andAnotherMessage = "Videos\tLabs\tZoom"; // use "\t" to add tabs in between words
+            System.out.println(andAnotherMessage);
+    }
 }
