26d7aafe304fa1b70e6bb7b1e62509de51d0f47b
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-04 11:29:10 -0600

    Create ForLoops.java

---
+++ Lab2/ForLoops.java
@@ -0,0 +1,17 @@
+package week2;
+
+public class ForLoops {
+    public static void main(String[] args) {
+
+
+
+        for (int x = 0 ; x < 5 ; x++ ) {
+            System.out.println(x+1);
+        }
+
+        for (int x=10; x<=0; x--){
+            System.out.println(x + "...");
+        }
+        System.out.println("Rocket Launch!!");
+    }
+}
