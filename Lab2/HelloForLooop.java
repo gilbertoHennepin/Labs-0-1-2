c1e9748aa86ca63e36ea8c472302fcf9cafc68da
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-04 11:29:46 -0600

    Create HelloForLooop.java

---
+++ Lab2/HelloForLooop.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class HelloForLoop {
+    public static void main(String[] args) {
+        for (int x = 0; x < 10; x = x+1) {
+            System.out.println(x);
+        }
+    }
+}
