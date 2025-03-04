80e6cb675f2cb9fa9348dc4e52ca2f7f134bf867
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-04 11:10:09 -0600

    Update Address.java

--- src/com/gil/Address.java
+++ src/com/gil/Address.java
@@ -1,4 +1,14 @@
 package com.gil;
 
 public class Address {
+    public static void main(String[] args) {
+
+//      String variable to store the data
+        String city = "Minneapolis";
+        String state = "mn";
+//      Make "state" into uppercase
+        String stateInUpperCase = state.toUpperCase();
+//      print city and then state in uppercase
+        System.out.println(city + " " + stateInUpperCase);
+    }
 }
