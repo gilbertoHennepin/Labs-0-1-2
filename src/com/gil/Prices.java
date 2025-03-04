bdf163f17d763b98a04c509efafb406ced209a63
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-04 11:10:54 -0600

    Update Prices.java

--- src/com/gil/Prices.java
+++ src/com/gil/Prices.java
@@ -1,4 +1,18 @@
 package com.gil;
 
 public class Prices {
+    public static void main(String[] args) {
+        String productName = "Broccoli";
+        double price = .99;
+        int quantity = 10;
+
+        double totalPrice = price * quantity;
+
+        System.out.println(quantity + " of " + productName
+                + "at $ " + price + " each costs a total of $ " + totalPrice);
+
+//        String formatting version for 2 decimal places in the prices
+        System.out.printf("%d of %s at $%.2f each costs a total of $%.2f",
+                quantity, productName, price, totalPrice);
+    }
 }
