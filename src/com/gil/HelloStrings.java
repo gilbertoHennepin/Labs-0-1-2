06e3735593184454ae01e06ef9fd5b9458774617
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-04 11:10:20 -0600

    Update HelloStrings.java

--- src/com/gil/HelloStrings.java
+++ src/com/gil/HelloStrings.java
@@ -1,4 +1,16 @@
 package com.gil;
 
 public class HelloStrings {
+
+    public static void main(String[] args) {
+        String name = "Gil";
+        String nameInUpperCase = name.toUpperCase();
+        System.out.println(nameInUpperCase);            //outputs the name in uppercase
+
+        String nameINLowerCase = name.toLowerCase();
+        System.out.println(nameINLowerCase);            // outputs the name in lowercase
+
+        int charactersInName = name.length();           // counts the characters in the input
+        System.out.println("There are "+ charactersInName + " characters in your name ");
+    }
 }
