7d87d040391b9dfd599eb596114ee0b7b7ed26ea
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-04 11:31:56 -0600

    Create WhileTrue.java

---
+++ Lab2/WhileTrue.java
@@ -0,0 +1,12 @@
+package week2;
+
+public class WhileTrue {
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Hello World");
+
+
+        }
+    }
+}
