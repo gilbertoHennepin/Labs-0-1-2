d9adced658af11f9b3977bbca086c91b719e8fef
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-04 11:10:44 -0600

    Update PizzaRolls.java

--- src/com/gil/PizzaRolls.java
+++ src/com/gil/PizzaRolls.java
@@ -1,4 +1,28 @@
 package com.gil;
 
-public class PizzaRolls {
+import java.sql.SQLOutput;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+//         create string variable
+        String name = "Gil"; //String is an Object type of integer
+//        print out the string variable
+            System.out.println("Hello " + name);
+
+        String college = "MCTC";
+//        print the data in a variable
+            System.out.println("Hello " + college);
+
+            System.out.println("My name is  " + name + " and my college is  " + college);
+
+        int javaCredits =6; // int works only works with whole numbers
+            System.out.println("The Java class is  " + javaCredits + " credits");
+
+//        floating point variable - double works with numbers that have decimal places
+        double todayTemp = 75.3;  //"double & int" is Primitive type of integer
+            System.out.println("The high temperature today is " + todayTemp + "F");
+    }
 }
